94376d8ea33d39402a8000cd970a0893e541dca8
suresh <budharapusuresh.8868@gmail.com>
2025-02-28 14:13:03 +0530

    update the terraform code

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "my_rg"
+location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "my-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  address_space = ["10.0.0.0/16"]
+
+}
